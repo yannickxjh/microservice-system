52a8aaaf1945d7e78a27a6eec314afff57f9b5ef
yannick <yannick.xu@epitech.eu>
2020-04-22 02:52:48 +0200

    WIP

--- ticket_order/Api/ticket.js
+++ ticket_order/Api/ticket.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
-//const knex = require('../database')
+const knex = require('../database')
 
 router.get('/test', (req, res) => {
 
 })
 
-// router.post('/ticket/order', (req, res) => {
-//     const user = req.body.userId
+router.post('/ticket/order', (req, res) => {
+    const user = req.body.userId
 
-//     knex('ticket')
-//         .returning('id')
-//         .insert([{ userId: user }, { status: 'onWaiting' }])
-// })
+    knex('ticket')
+        .returning('id')
+        .insert([{ userId: user }, { status: 'onWaiting' }])
+})
 
 module.exports = router
